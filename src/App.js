b12aab972d8a867398eedad20881d18f77cc7818
ImHANSOLO <1505470071@qq.com>
2024-07-07 03:33:49 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const Ref = useRef(null);
+  const [timer, setTimer] = useState("00:00:00");
+
+  const getTimeRemaining = (e) => {};
+
+  const startTimer = () => {};
+
+  const clearTimer = () => {};
+
+  const getDeadTime = () => {};
+};
 
 export default App;
